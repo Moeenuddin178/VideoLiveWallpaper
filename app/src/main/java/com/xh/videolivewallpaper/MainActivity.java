f0aca2a910452a78f39651dca042c7c0c292a048
icofans <565057208@qq.com>
2021-09-05 16:01:53 +0800

    first commit

---
+++ app/src/main/java/com/xh/videolivewallpaper/MainActivity.java
@@ -0,0 +1,41 @@
+package com.xh.videolivewallpaper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private CheckBox mCbVoice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCbVoice = (CheckBox) findViewById(R.id.id_cb_voice);
+
+        mCbVoice.setOnCheckedChangeListener(
+                new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(
+                            CompoundButton buttonView, boolean isChecked) {
+
+                        if (isChecked) {
+                            // 静音
+                            VideoLiveWallpaper.voiceSilence(getApplicationContext());
+                        } else {
+                            VideoLiveWallpaper.voiceNormal(getApplicationContext());
+                        }
+                    }
+                });
+    }
+
+    public void setVideoToWallPaper(View view) {
+        VideoLiveWallpaper.setToWallPaper(this);
+    }
+}
